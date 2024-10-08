4a1fc188e0531382856e39a5c7399fc5915b302e
Gatofonte <gatocall@hotmail.com>
2024-10-08 19:08:45 -0300

    punto inicial
    Version entregada en el TP2 mas un agregado de una clase para abrir un branch

---
+++ TP2_Peliculas/Clases.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Peliculas
+{
+    internal class Clases
+    {
+    }
+
+    public class Peliculas_cargadas
+    {
+        private string titulo { get; set; }
+        private string director { get; set; }
+        private string genero { get; set; }
+        private string actores { get; set; }
+        private int duracion {  get; set; }
+        private int realizacion { get; set; }
+        private DateOnly estreno {  get; set; }
+        private string filmacion { get; set; }
+        private string pais {  get; set; }
+
+        public Peliculas_cargadas(string titulo, string director, string genero, string actores,
+                                int duracion, int realizacion, DateOnly estreno, string filmacion, string pais)
+        {
+            this.titulo = titulo;
+            this.director = director;
+            this.genero = genero;
+            this.actores = actores;
+            this.duracion = duracion;
+            this.realizacion = realizacion;
+            this.estreno = estreno;
+            this.filmacion = filmacion;
+            this.pais = pais;
+        }
+    }
+}
